5e57de416d7dbfdb2d9861145bc181c982c84c42
SviatoslavJ <fwsviatoslav@gmail.com>
2024-11-17 22:28:27 +0100

    test fix

--- src/main/frontend/shopproject/src/app/page.tsx
+++ src/main/frontend/shopproject/src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 
 export default function Home() {
+
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -18,7 +20,6 @@ export default function Home() {
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
-            .
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
@@ -31,6 +32,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <Image
+
               className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
@@ -49,6 +51,7 @@ export default function Home() {
           </a>
         </div>
       </main>
+      
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -93,8 +96,9 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to nextjs.org
         </a>
+
       </footer>
     </div>
   );
