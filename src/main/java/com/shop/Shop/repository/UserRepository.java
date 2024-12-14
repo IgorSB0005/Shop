c2f7877422621fd1f0d8f65ac92431d6f75ab943
IgorSB0005 <igor.shpakov05@gmail.com>
2024-12-14 17:11:09 +0100

    add Repository
    add Repository for users

---
+++ src/main/java/com/shop/Shop/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.shop.Shop.repository;
+
+import com.shop.Shop.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
