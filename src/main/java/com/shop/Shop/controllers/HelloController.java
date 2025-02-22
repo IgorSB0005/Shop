7306acbe55788007835925293c1775a81c8c2a0c
IgorSB0005 <igor.shpakov05@gmail.com>
2025-02-22 19:14:37 +0100

    added a login system and fixed some bugs

--- src/main/java/com/shop/Shop/controllers/HelloController.java
+++ src/main/java/com/shop/Shop/controllers/HelloController.java
@@ -3,9 +3,12 @@ package com.shop.Shop.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
-    @GetMapping("/hello")
-        public String helloWorld() {
-            return "Hello world!";
-        }
+
+    @GetMapping("/home")
+    public String pageForUser(){
+        return "All Home";
+    }
+
 }
