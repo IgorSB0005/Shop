2f7ea0a92d7ca20850c78b7cff0b4b98817f09e1
SviatoslavJ <fwsviatoslav@gmail.com>
2025-02-23 18:57:08 +0100

    changes in header

--- src/main/frontend/shopproject/src/app/page.tsx
+++ src/main/frontend/shopproject/src/app/page.tsx
@@ -4,17 +4,22 @@ export default function Home() {
 
 
   return (
-    <div className="flex flex-col items-center justify-items-center min-h-screen  font-[family-name:var(--font-geist-sans)]">
-      <header className="flex flex-row justify-around p-10 bg-white text-black w-full">
-        <div>LOGO</div>
-        <div>New</div>
-        <div>Men</div>
-        <div>Women</div>
-        <div>Accessories</div>
+    <div className="flex flex-col items-center justify-items-center h-screen font-[family-name:var(--font-geist-sans)]">
+      <header className="text-black w-full">
+        
+        <img
+          alt="#"
+          src="/images/headerback.jpg"
+          className="-z-10 h-full w-full object-cover object-right md:object-center brightness-25"
+
+        />
       </header>
-      <main className="flex-1">
-        <div>
-          <img src="/images/modernbike.jpg" alt="#" />
+      <main className="">
+        <div className=" mb-10 border-t-4 border-b-4 rounded-lg shadow-[0_0_15px_5px_rgba(255,255,255,0.7)] w-screen">
+          <h1 className="text-5xl pt-20 pb-20">
+            Speed and technology, <br /> everything for the most modern
+            <br /> and environmentally friendly movement.
+          </h1>
         </div>
       </main>
 
